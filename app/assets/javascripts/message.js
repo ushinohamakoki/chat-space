f48fb3a57ef4c97cc3b061fea3fc33303425c5a8
ushinohamakoki <60771575+ushinohamakoki@users.noreply.github.com>
2020-03-10 15:03:11 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -19,7 +19,6 @@ $(function(){
           </div>
           <img src=${message.image} >
         </div>`
-      return html;
     } else {
       var html =
       `<div class="main__message-list>
@@ -37,8 +36,8 @@ $(function(){
           </p>
         </div>
       </div>`
-    return html;
     };
+    return html;
   } 
 
   $('#new_message').on('submit', function(e){
@@ -67,66 +66,3 @@ $(function(){
     return false;
   });
 });
-
-
-
-
-// $(function(){ 
-//   function buildHTML(message){
-//    if ( message.image ) {
-//      var html =
-//       `<div class="message">
-//          <div class="upper-message">
-//            <div class="upper-message__user-name">
-//              ${message.user_name}
-//            </div>
-//            <div class="upper-message__date">
-//              ${message.created_at}
-//            </div>
-//          </div>
-//          <div class="lower-message">
-//            <p class="lower-message__content">
-//              ${message.content}
-//            </p>
-//          </div>
-//          <img src=${message.image} >
-//        </div>`
-//      return html;
-//    } else {
-//      var html =
-//       `<div class="message">
-//          <div class="upper-message">
-//            <div class="upper-message__user-name">
-//              ${message.user_name}
-//            </div>
-//            <div class="upper-message__date">
-//              ${message.created_at}
-//            </div>
-//          </div>
-//          <div class="lower-message">
-//            <p class="lower-message__content">
-//              ${message.content}
-//            </p>
-//          </div>
-//        </div>`
-//      return html;
-//    };
-//  }
-// $('#new_message').on('submit', function(e){
-//  e.preventDefault();
-//  var formData = new FormData(this);
-//  var url = $(this).attr('action')
-//  $.ajax({
-//    url: url,
-//    type: "POST",
-//    data: formData,
-//    dataType: 'json',
-//    processData: false,
-//    contentType: false
-//  })
-//   .done(function(data){
-//     var html = buildHTML(data);
-//   })
-// })
-// });
-
