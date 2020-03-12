baa3fb37f404509ff8abf5ba0e81d6926d725ebf
ushinohamakoki <60771575+ushinohamakoki@users.noreply.github.com>
2020-03-12 10:27:20 +0900

    indexアクションの内容

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,5 +1,7 @@
 class Api::MessagesContoroller < ApplicationController
   def index
-
+    group = Group.find(params[:group_id])
+    last_message_id = params[:id].to_i
+    @messages = group.message.includes(:user).where("id > ?", last_message_id)
   end
 end
